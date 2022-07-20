4500881fdc042aeab184b80de1cf4a8dd0eeb252
JabDoesThings <JabDoesThings@github.com>
2022-07-20 17:37:16 -0500

    Documented `LuaElement`.

--- src/ts/lua/LuaElement.ts
+++ src/ts/lua/LuaElement.ts
@@ -1,12 +1,16 @@
 /**
- * **LuaElement**
+ * **LuaElement** is the base class for all compiled Lua elements in the generator.
  * 
  * @author JabDoesThings
  */
 export class LuaElement {
-    readonly name: string;
+  /** The name of the element. (If stored globally, identifies as such) */
+  readonly name: string;
 
-    constructor(name: string) {
-        this.name = name;
-    }
+  /**
+   * @param name The name of the element. (If stored globally, identifies as such)
+   */
+  constructor(name: string) {
+    this.name = name;
+  }
 }
