b8c15a6e4aac57928b3fbf9ac83daef173495eaf
JabDoesThings <JabDoesThings@github.com>
2022-07-20 17:41:50 -0500

    Documented `LuaField`.

--- src/ts/lua/LuaField.ts
+++ src/ts/lua/LuaField.ts
@@ -1,20 +1,27 @@
-import { LuaClass } from "./LuaClass";
-import { LuaElement } from "./LuaElement";
+import { LuaClass } from './LuaClass';
+import { LuaElement } from './LuaElement';
 
 /**
- * **LuaField**
- * 
+ * **LuaField** stores calls to constants or variables.
+ *
+ * LuaFields are treated as fields for classes and treated as properties for tables and global.
+ *
  * @author JabDoesThings
  */
 export class LuaField extends LuaElement {
+  /** (Optional) The class the field is assigned to. */
+  readonly clazz: LuaClass | null;
+  /** (Optional) If assigned to a class, this tells the generator if the field should be accessed statically or accessed only from a class instance. */
+  readonly isStatic: boolean;
 
-    readonly clazz: LuaClass;
-    readonly isStatic: boolean;
-
-    constructor(clazz: LuaClass, name: string, isStatic: boolean) {
-        super(name);
-
-        this.clazz = clazz;
-        this.isStatic = isStatic;
-    }
+  /**
+   * @param clazz (Optional) The class the field is assigned to.
+   * @param name The name of the element. (If stored globally, identifies as such)
+   * @param isStatic (Optional) If assigned to a class, this tells the generator if the field should be accessed statically or accessed only from a class instance.
+   */
+  constructor(clazz: LuaClass | null, name: string, isStatic: boolean = true) {
+    super(name);
+    this.clazz = clazz;
+    this.isStatic = isStatic;
+  }
 }
