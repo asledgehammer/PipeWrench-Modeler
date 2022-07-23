9b98fdd7fae322a3b78fb9d195314397357cc8ec
JabDoesThings <JabDoesThings@github.com>
2022-07-22 21:44:43 -0500

    Progress on UI design.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,23 +1,26 @@
 import * as hljs from 'highlight.js';
 import { LuaLibrary } from './lua/LuaLibrary';
-import { ModelLibrary } from './lua/model/ModelLibrary';
 
 const luaLibrary = new LuaLibrary();
 
 // Entry Point from HTML.
 export let start = function () {
-  // const code = fs.readFileSync('./assets/media/ISUIElement.lua').toString();
-  // const html = hljs.default.highlight(code, {language: 'lua'}).value;
-  // let s = '<pre><code class="hljs language-lua">' + html + '</code></pre>'
-  // document.getElementById('code').innerHTML = s;
+  setTimeout(() => {
+    luaLibrary.scan();
+    luaLibrary.parse();
 
-  luaLibrary.scan();
-  luaLibrary.parse();
-  
-  const code = luaLibrary.classes['ISUIElement'].compile();
-  console.log(code);
+    
+    // const listItems = document.getElementById("class-list");
+    // const classKeys = Object.keys(luaLibrary.classes);
+    // classKeys.sort((o1, o2) => o1.localeCompare(o2));
+    // for (const key of classKeys) {
+      // const s = `<div class="item"><div class="hover-cube"></div><label>${key}</label></div>`;
+      // listItems.innerHTML += s;
+    // }
 
-  const html = hljs.default.highlight(code, {language: 'typescript'}).value;
-  let s = '<pre><code class="hljs language-typescript">' + html + '</code></pre>'
-  document.getElementById('code').innerHTML = s;
+    const code = luaLibrary.classes['ISUIElement'].compile();
+    const html = hljs.default.highlight(code, {language: 'typescript'}).value;
+    let s = '<pre><code class="hljs language-typescript">' + html + '</code></pre>'
+    document.getElementById('code').innerHTML += s;    
+  }, 100);
 };
